1e582863c7b6b103ae5a8d8e1601c6e6fde043fc
mayank-sheoran <50839793+mayank-sheoran@users.noreply.github.com>
2020-06-24 02:45:37 +0530

    update

---
+++ app-main.js
@@ -0,0 +1,9 @@
+// setTimeout(function(){ 
+//     $('.loader-wrap').css('display', 'none');
+// }, 3000); 
+
+setTimeout(function(){ 
+    $('.website').css('position', 'static');
+}, 2000);
+
+// setTimeout(function(){ $(elem).css('z-index','2'); },2000)
